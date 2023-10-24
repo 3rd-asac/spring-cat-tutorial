034261941f803da57e496d71e3415ad0e9f7b284
ddongbu <sangmin5848@naver.com>
2023-10-24 09:50:13 +0900

    Basic Setting

---
+++ src/test/java/com/coupang/demo/ErdcoupangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coupang.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErdcoupangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
