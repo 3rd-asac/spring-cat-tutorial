d99e4bd65a68669cfe74c680687869fac622493f
Asac_Full <rladnwls0523@naver.com>
2023-10-26 10:42:27 +0900

    Feat: 고양이 이미지 기능 추가, index.html 생성

--- src/main/java/com/asac/catapiapp/controller/CatController.java
+++ src/main/java/com/asac/catapiapp/controller/CatController.java
@@ -1,20 +1,37 @@
 package com.asac.catapiapp.controller;
 
+import com.asac.catapiapp.model.CatResponse;
 import com.asac.catapiapp.service.CatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class CatController {
 
-    @Autowired
-    private CatService catService;
+    private final CatService catService;
 
-    @GetMapping("/random-cat")
-    public ResponseEntity<String> getRandomCat() {
-        String imageUrl = catService.getRandomCatImage();
-        return ResponseEntity.ok(imageUrl);
+    @Autowired
+    public CatController(CatService catService){
+        this.catService = catService;
+    }
+
+    @GetMapping("/")
+    public String main(){
+        return "index";
+    }
+
+    @GetMapping("/Image/cat")
+    public String getRandomCat(@RequestParam(value = "limit", defaultValue = "1") String limit, Model model) {
+        System.out.println(limit);
+        CatResponse[] catResponses = catService.getRandomCatImage(limit);
+        for(CatResponse c : catResponses)
+            System.out.println(c.getUrl());
+        model.addAttribute("catImages", catResponses);
+        return "index";
     }
 }
--- src/main/java/com/asac/catapiapp/service/CatService.java
+++ src/main/java/com/asac/catapiapp/service/CatService.java
@@ -7,29 +7,41 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @Service
 public class CatService {
 
-    @Value("${catapi.key}")
+    @Value("${api.key}")
     private String API_KEY;
 
     private final String CAT_API_URL = "https://api.thecatapi.com/v1/images/search";
 
-    public String getRandomCatImage() {
+    public CatResponse[] getRandomCatImage(String limit) {
         RestTemplate restTemplate = new RestTemplate();
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://api.thecatapi.com") //http://localhost에 호출
+                .path("/v1/images/search")
+                .queryParam("limit", limit)  // query parameter가 필요한 경우 이와 같이 사용
+                .encode()
+                .build()
+                .toUri();
 
         HttpHeaders headers = new HttpHeaders();
         headers.set("x-api-key", API_KEY);
         HttpEntity<?> entity = new HttpEntity<>(headers);
 
-        ResponseEntity<CatResponse[]> response = restTemplate.exchange(CAT_API_URL, HttpMethod.GET, entity, CatResponse[].class);
+        ResponseEntity<CatResponse[]> response = restTemplate.exchange(uri, HttpMethod.GET, entity, CatResponse[].class);
         CatResponse[] catResponses = response.getBody();
 
         // 예외 처리를 추가로 NullPointerException 방지
         if(catResponses != null && catResponses.length > 0) {
-            return catResponses[0].getUrl();
+            return catResponses;
         }
 
         return null; // 또는 기본 이미지 URL을 반환
